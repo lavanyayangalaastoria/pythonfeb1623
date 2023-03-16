7f7da7f71255c1167a27d76e0d309ef8d4e01ef9
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-16 15:37:00 +0530

    added variable names conventions

--- variables.py
+++ variables.py
@@ -11,3 +11,17 @@ print(type(y))
 print(type(z))
 #python is case sensitive
 # char can be created using single or double quotesk
+
+# variable names
+# variable names cannot start with numbers
+my_var = "Hi"
+my_var2 = "from John"
+print(my_var + " "+ my_var2)
+
+#types of variable names conventions
+
+camelcase = "camelcaseconvention"
+PascalCase = "PascalCaseConvention"
+snake_case = "snakecase_convention"
+print(camelcase,PascalCase,snake_case)
+
