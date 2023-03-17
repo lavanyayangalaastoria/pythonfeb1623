485526a0910b6ad038c0076ccf03cd7084f83751
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 12:13:21 +0530

    float type desc

--- numbers.py
+++ numbers.py
@@ -14,3 +14,26 @@ print(x,y,z)
 print(type(x))
 print(type(y))
 print(type(z))
+print("1. int")
+# 1 int
+x = 1
+y = 33333
+z = -1111
+print(type(x))
+print(type(y))
+print(type(z))
+print("2.float")
+# 2 float 
+x = 1.10 
+y = 1.0 
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+print("Float can also be scientific numbers with 'e' which indicates power of 10")
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
