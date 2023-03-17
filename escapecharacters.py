96a426cdc58b2a9ec84ed2d402c0661720dab837
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 14:27:09 +0530

    escape charac

---
+++ escapecharacters.py
@@ -0,0 +1,11 @@
+print("Escape Characters")
+#To insert characters that are illegal in a string escape character can be used.
+# \' 	Single Quote 	
+# \\ 	Backslash 	
+# \n 	New Line 	
+# \r 	Carriage Return 	
+# \t 	Tab 	
+# \b 	Backspace 	
+# \f 	Form Feed 	
+# \ooo 	Octal value 	
+# \xhh 	Hex value
