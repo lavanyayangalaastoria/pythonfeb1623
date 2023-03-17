fe05a0fc9c268b8ee20ac7efc706320ae9305e24
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 14:54:54 +0530

    loops programs added

---
+++ loops.py
@@ -0,0 +1,53 @@
+print(" if condition")
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+print(" 2. elif")
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+print("3.Else condition")
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+  
+print("OR operator")
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+  
+print("NOT Operator")
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+  
+print("Nested if statements")
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+    
+print("Pass statement")
+a = 33
+b = 200
+
+if b > a:
+  pass
