4f7b387ae5f3eaa3a6caa37950832b4da2fd4fa4
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 16:05:45 +0530

    class file

---
+++ class.py
@@ -0,0 +1,9 @@
+class Bike:
+    name = ""
+    gear = 0
+
+bike1 = Bike() #creating object to class
+bike1.gear = 11 #accessing data from class
+bike1.name = "Mountain Bike"
+
+print(f"Name: {bike1.name}, Gears: {bike1.gear} ")
