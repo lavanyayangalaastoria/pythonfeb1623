b6fb4ff216fad1b6f055fa13bd2ab64e897fa15b
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 12:08:15 +0530

    numeric types and type()

--- numbers.py
+++ numbers.py
@@ -1,3 +1,16 @@
 #numbers in python
 print("numbers in python")
 
+# there are three numeric types in python.
+# 1 int
+# 2 float
+# 3 complex
+# variables of numeric types are created when you assign a value to them.
+
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
