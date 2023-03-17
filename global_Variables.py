efd2a3531f7e7b1ad3dafba15f0a175c36db596a
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 11:04:14 +0530

    global variables ex 1

---
+++ global_Variables.py
@@ -0,0 +1,12 @@
+# variables created outside a function called as global variables.
+# they can be used by everyone inside and outside of a function
+# local variables are variables which are within the function where variables have been declared.
+# global variables - entire program
+
+a = 20
+def display():
+    a = 50
+    print("Local value is",a)
+display()
+print("Global value is",a)
+
