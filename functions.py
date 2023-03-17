eeaf9870142b320b8fc3a564180f748f1d8e22a6
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 15:34:16 +0530

    functions file 1

---
+++ functions.py
@@ -0,0 +1,15 @@
+print(" --- Functions --- ")
+print("--Creating a Function--")
+def myfunction():
+    print("2. my function is created")
+
+def myfunction():
+    print("my function is called")
+myfunction()
+print("--Passing Arguments--")
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
