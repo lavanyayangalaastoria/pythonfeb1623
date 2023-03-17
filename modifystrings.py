4c80a080d09fd9f01bbc89a5b44b0fa5f2f9687c
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-17 12:46:45 +0530

    modify strings

---
+++ modifystrings.py
@@ -0,0 +1,15 @@
+print("Uppercase")
+a = "Hello, World!"
+print(a.upper())
+print("Lowercase")
+a = "Hello, World!"
+print(a.lower())
+print("strip removes white space from begining or end")
+a = "  Hello,World!"
+print(a.strip())
+print("Replace string")
+a = "Hello world"
+print(a.replace("H","J"))
+print("Split string")
+a = "Hello, World!"
+print(a.split(","))
