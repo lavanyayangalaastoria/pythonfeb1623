d34318197553691a82a10fd71084e97dd6311ee9
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-20 10:26:15 +0530

    Updated headings and code

--- slicingstrings.py
+++ slicingstrings.py
@@ -1,9 +1,9 @@
 print("Slicing is used to return range of characters using the slice syntax")
 b = "Hello, World!"
 print(b[2:5])
-print("Slicing from start")
+print("--Slicing from start--")
 print(b[:5])
-print("Slicing from end")
-print(b[:2])
-print("Negative indexing")
+print("--Slicing to the end--")
+print(b[2:])
+print("--Negative indexing--")
 print(b[-5:-2])
