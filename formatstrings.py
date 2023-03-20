a0506e07dbdce79787a8f21d00720be32411dab8
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-20 10:38:48 +0530

    updated statement

--- formatstrings.py
+++ formatstrings.py
@@ -2,7 +2,7 @@ print("format() method is usedd to pass arguments ,formmat them and add the valu
 age = 36
 txt = "John's age is {}"
 print(txt.format(age))
-#format() method takes unlimited num of arguments and are placed to respective placeholders
+print("format() method takes unlimited num of arguments and are placed to respective placeholders")
 quantity = 3
 itemno = 567
 price = 49.95
