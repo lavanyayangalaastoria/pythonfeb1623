e8b3b5b4dec4f88285e946451627f471fa928c32
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-20 11:27:55 +0530

    added some code in booleans

--- booleans.py
+++ booleans.py
@@ -4,14 +4,14 @@ print(10 == 9)
 print(10 < 9) 
 a = 200
 b = 33
-
+print("--example two--")
 if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a") 
   
 print("Evaluate string and a number:")
-  
+print("--bool() funcn allows to evaluate any value - returns true or false in return--")
 print(bool("Hello"))
 print(bool(15))
 
@@ -22,11 +22,11 @@ y = 15
 print(bool(x))
 print(bool(y))
 
-print("Most values are True: ")
+print("--Most values are True:-- ")
 print(bool("abc"))
 print(bool(123))
 print(bool(["apple", "cherry", "banana"]))
-print("Most values are false: ")
+print("--Most values are false: --")
 print(bool(False))
 print(bool(None))
 print(bool(0))
@@ -34,12 +34,18 @@ print(bool(""))
 print(bool(()))
 print(bool([]))
 print(bool({}) )
-
-#functions can return boolean
+print("One more value, or object in this case, evaluates to False, and that is if you have an object that is made from a class with a __len__ function that returns 0 or False:")
+class myclass():
+  def __len_(self):
+    return 0 
+myobj = myclass()
+print(bool(myobj))
+print("--functions can return boolean--")
 def myFunction() :
   return True
 
 print(myFunction())
+print("print 'YES!' if the function returns True, otherwise print 'NO!' ")
 def myFunction() :
   return True
 
