995a79a0ef5b1602028a18bdafc17bcdbdd1ae4f
lavanya.y <lavanya.y@astoriasolutions.com>
2023-03-20 11:13:27 +0530

    string methods added 5 remaining are pending

--- string_methods.py
+++ string_methods.py
@@ -1,5 +1,79 @@
 print("--String Methods--")
-print("--capitalize() --")
+print("--1.capitalize() --")
+print("--Upper case the first letter of the sentence--")
 txt = "hello, and welcome to my world."
 x = txt.capitalize()
 print("Output of capitalize() is:"+x)
+print("--If first character is number nothing happens-- ")
+txt = "36 is my age"
+x = txt.capitalize()
+print(x)
+print("2.casefold() method")
+txt = "Hello, And Welcome To My World!"
+print("--casefold() returns a string where characters are lower case--")
+x = txt.casefold()
+
+print(x)
+print("3.# center()	Returns a centered string ")
+txt = "banana"
+
+x = txt.center(20)
+
+print(x) 
+print("4 count()	Returns the number of times a specified value occurs in a string")
+txt = "I love apples, apple are my favorite fruit"
+
+x = txt.count("apple")
+
+print(x) 
+print("5.encode()	Returns an encoded version of the string")
+txt = "My name is Ståle"
+
+x = txt.encode()
+
+print(x)
+# Method 	Description
+# capitalize()	Converts the first character to upper case
+# casefold()	Converts string into lower case
+# center()	Returns a centered string
+# count()	Returns the number of times a specified value occurs in a string
+# encode()	Returns an encoded version of the string
+# endswith()	Returns true if the string ends with the specified value
+# expandtabs()	Sets the tab size of the string
+# find()	Searches the string for a specified value and returns the position of where it was found
+# format()	Formats specified values in a string
+# format_map()	Formats specified values in a string
+# index()	Searches the string for a specified value and returns the position of where it was found
+# isalnum()	Returns True if all characters in the string are alphanumeric
+# isalpha()	Returns True if all characters in the string are in the alphabet
+# isdecimal()	Returns True if all characters in the string are decimals
+# isdigit()	Returns True if all characters in the string are digits
+# isidentifier()	Returns True if the string is an identifier
+# islower()	Returns True if all characters in the string are lower case
+# isnumeric()	Returns True if all characters in the string are numeric
+# isprintable()	Returns True if all characters in the string are printable
+# isspace()	Returns True if all characters in the string are whitespaces
+# istitle() 	Returns True if the string follows the rules of a title
+# isupper()	Returns True if all characters in the string are upper case
+# join()	Joins the elements of an iterable to the end of the string
+# ljust()	Returns a left justified version of the string
+# lower()	Converts a string into lower case
+# lstrip()	Returns a left trim version of the string
+# maketrans()	Returns a translation table to be used in translations
+# partition()	Returns a tuple where the string is parted into three parts
+# replace()	Returns a string where a specified value is replaced with a specified value
+# # rfind()	Searches the string for a specified value and returns the last position of where it was found
+# # rindex()	Searches the string for a specified value and returns the last position of where it was found
+# rjust()	Returns a right justified version of the string
+# rpartition()	Returns a tuple where the string is parted into three parts
+# rsplit()	Splits the string at the specified separator, and returns a list
+# rstrip()	Returns a right trim version of the string
+# split()	Splits the string at the specified separator, and returns a list
+# splitlines()	Splits the string at line breaks and returns a list
+# startswith()	Returns true if the string starts with the specified value
+# strip()	Returns a trimmed version of the string
+# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+# title()	Converts the first character of each word to upper case
+# translate()	Returns a translated string
+# upper()	Converts a string into upper case
+# zfill()	Fills the string with a specified number of 0 values at the beginning
